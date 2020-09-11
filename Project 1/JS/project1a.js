be2a8eacc9fd3fc9e0290146737bec7f72a0e172
jakejose <jakejose.iu.edu>
2020-09-11 15:41:31 -0400

    class changes

--- Project 1/JS/project1a.js
+++ Project 1/JS/project1a.js
@@ -1,2 +1,24 @@
 'use strict';
 console.log('working');
+
+//1 select the HTML Elements
+//p, button, body
+
+//2 connect button to function with an event listener
+
+
+//3 write the function
+//4 picks a color (A,B,C)
+    //array named colors of colors
+//5 update the HTML (textContent (or innerHTML), style)
+
+
+
+
+
+function toCallback() {
+
+}
+
+
+HTMLElement.addEventListener('click',toCallback)
