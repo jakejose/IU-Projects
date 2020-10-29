4bb8c20a9717ba2245e48518d006d955e0f87395
jakejose <jakejose@iu.edu>
2020-10-28 21:59:11 -0400

    Adding some images and more JS/CSS

--- Project 3/project3.js
+++ Project 3/project3.js
@@ -1,57 +1,75 @@
 'use strict';
 console.log('working');
 
-function loadJSON(callback) {   
+function loadJSON(callback) {
     var xobj = new XMLHttpRequest();
     xobj.overrideMimeType("application/json");
     xobj.open('GET', 'project3.json', true);
     xobj.onreadystatechange = function () {
-      if (xobj.readyState == 4 && xobj.status == "200") {
-        callback(JSON.parse(xobj.responseText));
-      }
+        if (xobj.readyState == 4 && xobj.status == "200") {
+            callback(JSON.parse(xobj.responseText));
+        }
     };
-    xobj.send(null);  
+    xobj.send(null);
 }
 
 let quiz = document.querySelector('.quiz');
-let buttons = document.querySelectorAll('button');
+
+
 
 let questionIds = [];
+let checked = [];
 let randomNum = "";
-console.log(questionIds);
+//console.log(questionIds);
 
-function pusher (data) {
-  data.forEach(element=>{
-      questionIds.push(element.id);
-      console.log(questionIds);
-  })
-  randomNum += questionIds[Math.floor(Math.random()*questionIds.length)];
-  console.log(randomNum);
+function pusher(data) {
+    data.forEach(element => {
+        questionIds.push(element.id);
+        //console.log(questionIds);
+    });
+    randomNum += questionIds[Math.floor(Math.random() * questionIds.length)];
+    console.log(randomNum);
+}
+//clear question
+//do not allow same question
+//move on to next queston
+function next() {
+    console.log(checked);
 }
 
 //inserting data into quiz
-function showQuiz(data){
-    let q = '';
+function showQuiz(data) {
+    let q = '<ul>';
     pusher(data);
     data.forEach(element => {
-        if(element.id === randomNum){
-        q +=
-        `<h2>${element.question}</h2>;
-        `
-        for (const x in element.answers){
-            q+=
-            `<button id = '${x}'>${x}: ${element.answers[x]}</button>`;
+        if (element.id === randomNum) {
+            q +=
+                `<h2>${element.question}</h2>
+        `;
+            for (const x in element.answers) {
+                q +=
+                    `<li><label for='${x}'>${x}: ${element.answers[x]}</label><br>
+            <input class = 'image' type='image' id = '${x}' src = 'images/${element.answers[x]}.png'>
+            </li>`;
+            }
+            checked.push(element.id);
         }
-    }
     });
-    quiz.insertAdjacentHTML('afterbegin',q);
-
+    quiz.insertAdjacentHTML('afterbegin', q + "</ul>");
+    let images = document.querySelectorAll('.image');
+    clicker(images);
+    
 }
 
+function clicker(items){
+    items.forEach(e =>{
+        e.addEventListener('click', ()=>{
+            console.log(checked);
+        })
+    })
+}
 
-
-
-loadJSON(function(json) {
+loadJSON(function (json) {
     //console.log(json);
     // do something with data
     showQuiz(json);
