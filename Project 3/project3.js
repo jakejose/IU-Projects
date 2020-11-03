dd94b4d54884d22c02579fc9241607330aead940
Jacob Joseph <jakejose@iu.edu>
2020-11-02 19:02:49 -0500

    IMG upload and js/json modification

--- Project 3/project3.js
+++ Project 3/project3.js
@@ -14,6 +14,7 @@ function loadJSON(callback) {
 }
 
 let quiz = document.querySelector('.quiz');
+let circles = document.querySelector('.marker');
 //questions list
 let questions = [];
 let random = [];
@@ -23,17 +24,37 @@ let numberAnswered = 0;
 let numberCorrect = 0;
 //push json into a list 
 function pusher(data){
+let marker = '';
   data.forEach(item => {
+    marker += `<div class ='circle' name = ${item.correct}></div>`
     questions.push(item);
-    
   });
+randomList(questions);
+circles.insertAdjacentHTML("afterbegin",marker);
 showQuiz(questions);
 }
+//shuffling the questions array to produce a random question
+//source https://www.w3resource.com/javascript-exercises/javascript-array-exercise-17.php
+function randomList(ls){
+    let num = ls.length,hold,index;
+    while (num>0){
+        index = Math.floor(Math.random() * num);
+        num--;
+        hold = ls[num];
+        ls[num]=ls[index];
+        ls[index]=hold;
+    }
+    return ls;}
 //display data
 function showQuiz(questions){
-    random.push(questions[Math.floor(Math.random() * questions.length)]);
+    console.log(questions);
+    random.push(questions[numberAnswered]);
+    console.log(random);
+    //random.push(questions[Math.floor(Math.random() * questions.length)]);
     let q = '<ul>';
+    
     questions.forEach(e =>{
+
         if (random.includes(e)&&checked.includes(e)===false) {
             q +=
                 `<center>
@@ -49,6 +70,7 @@ function showQuiz(questions){
             } 
             checked.push(e);
             correct += e.correct;
+
         } 
 
     });
@@ -58,14 +80,21 @@ function showQuiz(questions){
     clicker(images);}
 
 
+
 function clicker(items){
     items.forEach(e =>{
         e.addEventListener('click', ()=>{
             numberAnswered += 1
+            let answer = document.querySelector(`div[name = '${correct}']`);
+            console.log(answer);
             if(correct.includes(e.id)){
+                answer.style.backgroundColor = "green";
                 numberCorrect += 1
                 console.log(numberCorrect);
                 console.log(numberAnswered);
+                console.log(e.id);
+            }else{
+                answer.style.backgroundColor = "red";
             }
             next();
         })
