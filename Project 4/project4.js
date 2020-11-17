0eb497d13fd28c43cd8e0bf2c4dcc41612e8557b
Jacob Joseph <jakejose@iu.edu>
2020-11-16 21:53:33 -0500

    Initial API fetch and connection test (google font added as well in HTML/CSS)

--- Project 4/project4.js
+++ Project 4/project4.js
@@ -1,2 +1,16 @@
 'use strict';
 console.log('working!');
+
+const getData = async league => {
+    const response = await fetch(`https://www.thesportsdb.com/api/v1/json/1/lookup_all_teams.php?id=${league}`);
+    if (!response.ok){
+        console.log('error grabbing API');
+        return;
+    }
+
+    const data = await response.json();
+
+    console.log(data);
+}
+
+getData('4391');
