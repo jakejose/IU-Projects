d33312f2c2293eb05173bd95041aa56f0295334c
Jacob Joseph <jakejose@iu.edu>
2020-11-18 14:04:27 -0500

    Integrating descriptions of teams from API

--- Project 4/project4.js
+++ Project 4/project4.js
@@ -29,7 +29,7 @@ const getData = async league => {
         let t= '';
         let name = team.strTeam;
         let badge = team.strTeamBadge;
-        let id = team.idTeaml
+        let id = team.idTeam
         t += `<div class = 'images' id = '${id}'>
         <h4>${name}</h4>
         <img src = ${badge}>
@@ -80,12 +80,49 @@ function show (){
 
 
 
-function cardClicker(){
-    let inner = document.querySelector('.inner');
+function cardClicker(description){
     let teams = document.querySelectorAll('.images');
+    let curTeamID = 0;
+    let i = '';
     teams.forEach(team =>{
         team.addEventListener('click',()=>{
-            console.log('hello');
+            curTeamID = parseInt(team.id);
+            console.log(curTeamID);
+            getDetails(curTeamID);
         })
-    })
+        })
+    }
+
+let inner = document.querySelector('.inner');
+let outer = document.querySelector('.outer');
+const getDetails = async details =>{
+    const response = await fetch(`https://www.thesportsdb.com/api/v1/json/1/lookupteam.php?id=${details}`);
+    if (!response.ok){
+        console.log('error grabbing API');
+        return;
+    }
+    const data = await response.json();
+    let newData = data.teams;
+    console.log(newData[0]);
+
+    let innerDes =
+    `<h3>${newData[0].strTeam} (${newData[0].strTeamShort})</h3>
+        <h3> ${newData[0].strLeague}</h3>
+        <h4>Location: ${newData[0].strStadiumLocation}</h4>
+        <h4>Stadium: ${newData[0].strStadium}</h4>
+        <h4>Year Founded: ${newData[0].intFormedYear}</h4>
+    ` 
+    inner.innerHTML = innerDes; 
+    outer.classList.add('open');
 }
+
+outer.addEventListener('click',()=>{
+    outer.classList.remove('open');
+  })
+  
+  window.addEventListener('keydown', (event)=>
+  {
+    if(event.keyCode == 27){
+    outer.classList.remove('open');
+    }
+})
