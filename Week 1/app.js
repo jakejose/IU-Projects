3c5c611b87da3e71f42ee757cca727820e4de0d2
Jacob Joseph <jakejose@iu.edu>
2020-12-30 19:48:50 -0800

    deleting and renaming

--- Week 1/app.js
+++
@@ -1,9 +0,0 @@
-function vowelPrinter() {
-    let word = input('Please enter word: ')
-    string = ''
-        if (word[i] === 'a'|| word[i] === 'e'|| word[i] === 'i'||word[i] === 'o'||word[i] === 'u'){
-            string += word[i]
-            string += " "
-        }console.log(string)
-    }
-vowelPrinter()
