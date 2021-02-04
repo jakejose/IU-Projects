0027529387617124618494ea4ffe8c0d34bda0fa
Jacob Joseph <jakejose@iu.edu>
2021-02-04 16:00:49 -0500

    messing around

---
+++ Capstone/rivetGrid.js
@@ -0,0 +1,10 @@
+'use strict';
+
+console.log('working');
+const menu = document.querySelectorAll('.rvt-dropdown__menu > button');
+
+console.log(menu);
+// if button is in the menu and is clicked 
+//add text content into search bar
+
+menu.forEach()
