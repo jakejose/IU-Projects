61cabdf281a1d439e0410f2418f3ceb95e1e704a
Jacob Joseph <jakejose@iu.edu>
2021-02-05 12:45:12 -0500

    js chage

--- Capstone/rivetGrid.js
+++ Capstone/rivetGrid.js
@@ -1,8 +1,8 @@
 'use strict';
 
 console.log('working');
-// if button is in the menu and is clicked 
-// add text content into search bar value
+/* // if button is in the menu and is clicked 
+// add text content into search bar value */
 const menu = document.querySelectorAll('.rvt-dropdown__menu > button');
 const search = document.querySelector('#search');
 const searchButton = document.querySelector('#searchButton');
@@ -12,19 +12,19 @@ menu.forEach(button => {
         search.value = button.textContent;
     })
 });
-//escape button exits modal
+/* //escape button exits modal */
 window.addEventListener('keydown',(event)=>{
     if(event.keyCode == 27 && modalEscape){
         modalEscape.click();
     }
 });
-//enter key activates search button
+/* //enter key activates search button */
 window.addEventListener('keydown',(event)=>{
     if(event.keyCode == 13){
         searchButton.click();
     }
 });
-//tester
+/* //tester */
 searchButton.addEventListener('click', ()=>{
     console.log('searchButton clicked')
 });
